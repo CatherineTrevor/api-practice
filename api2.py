21cc9289a99d83a78b910de80b18ec96488960d2
Catherine Trevor <catherinetrevor@outlook.com>
2021-06-07 18:21:47 +0000

    another api example

---
+++ api2.py
@@ -0,0 +1,43 @@
+
+# Python program to convert the currency
+# of one country to that of another country 
+  
+# Import the modules needed
+import requests
+
+
+class Currency_convertor:
+    # empty dict to store the conversion rates
+    rates = {} 
+    def __init__(self, url):
+        data = requests.get(url).json()
+
+
+        # Extracting only the rates from the json data
+        self.rates = data["rates"] 
+
+
+    # function to do a simple cross multiplication between 
+    # the amount and the conversion rates
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        if from_currency != 'EUR' :
+            amount = amount / self.rates[from_currency]
+  
+        # limiting the precision to 2 decimal places
+        amount = round(amount * self.rates[to_currency], 2)
+        print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
+
+
+# Driver code
+if __name__ == "__main__":
+  
+
+    # YOUR_ACCESS_KEY = 'GET YOUR ACCESS KEY FROM fixer.io'
+    url = 'http://data.fixer.io/api/latest?access_key=5eed72b7e82098a9912a4ebbf64bb4c8&format=1'
+    c = Currency_convertor(url)
+    from_country = input("From Country: ")
+    to_country = input("TO Country: ")
+    amount = int(input("Amount: "))
+
+    c.convert(from_country, to_country, amount)
